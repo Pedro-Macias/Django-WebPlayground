492cc673ac43ee1db8cc78023102a69572b6ed0c
Pedro Macias <pin.macias@gmail.com>
2021-04-25 12:21:59 +0200

    trasformar en vistas basadas en clases

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+# importamos las vistas de core/views
+from .views import HomePageView , SamplePageView
 
+# https://docs.djangoproject.com/en/3.1/ref/class-based-views/base/#django.views.generic.base.TemplateView
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('sample/', views.sample, name="sample"),
+    path('', HomePageView.as_view(), name="home"),
+    path('sample/', SamplePageView.as_view(), name="sample"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
+# trasformar en vistas basadas en clases
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+# https://docs.djangoproject.com/en/3.1/ref/class-based-views/base/#django.views.generic.base.TemplateView
+from django.views.generic.base import TemplateView
 
-def sample(request):
-    return render(request, "core/sample.html")
+
+# traformar la funcion en una clase
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    # procesar la respuesta de la vista
+    def get(self,request, *args , **kwargs):
+        return render(request, self.template_name,{'title':'Esta es mi web Playground'})
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
